c565fc63fa0bb204a2836abcd64521632b259a76
Kiplangat001 <123592210+Kiplangat001@users.noreply.github.com>
2023-01-25 23:50:46 +0300

    Add files via upload

---
+++ Program for finding product.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+int x,y,PRODUCT;
+printf("enter the value of x\n");
+scanf("%d",&x);
+printf("enter the value of y\n");
+scanf("%d",&y);
+PRODUCT=x*y;
+printf("The product of x and y is %d", PRODUCT);
+return 0;
+}
---
+++ program for finding area of a circle.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    float r,area;
+    printf("enter value of r\n");
+    scanf("%f",&r);
+    area=3.142*r*r;
+    printf("The area of circle is %.0f", area);
+    return 0;
+
+}
